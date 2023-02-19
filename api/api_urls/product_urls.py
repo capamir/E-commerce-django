c310c5e5847d8d2bad651db58db4eb4a87e1e6dd
amirhossein <79211332+capamir@users.noreply.github.com>
2023-02-19 12:32:51 -0500

    admin delete product

--- api/api_urls/product_urls.py
+++ api/api_urls/product_urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.get_products, name='get-products'),
     path('<int:product_id>/', views.get_product_details, name='get-product-details'),
 
+    # Admin
+    path('delete/<int:product_id>/', views.delete_product, name='admin-delete-product'),
+    
 ]
--- api/views/product_views.py
+++ api/views/product_views.py
@@ -1,5 +1,7 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import status
 
 from products.models import Product
 from api.serializers import ProductSerializer
@@ -17,4 +19,15 @@ def get_product_details(request, product_id):
     product = Product.objects.get(id=product_id)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
-    
+
+@api_view(['DELETE'])  
+@permission_classes([IsAdminUser])
+def delete_product(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+        product.delete()
+
+        return Response('product deleted successfully')
+    except:
+        message = {'derail': 'Product not found'}
+        return Response(message, status=status.HTTP_404_NOT_FOUND)
