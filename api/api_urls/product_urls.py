3757067ad5f5ad8304fc9ae24b542c79e041dfb5
amirhossein <79211332+capamir@users.noreply.github.com>
2023-02-19 19:43:39 -0500

    review

--- api/api_urls/product_urls.py
+++ api/api_urls/product_urls.py
@@ -4,7 +4,8 @@ from api.views import product_views as views
 urlpatterns = [
     path('', views.get_products, name='get-products'),
     path('<int:product_id>/', views.get_product_details, name='get-product-details'),
-
+    path('<int:product_id>/reviews/', views.create_product_review, name='create-product-review'),
+    
     # Admin
     path('create/', views.create_product, name='admin-create-product'),
     path('update/<int:product_id>/', views.update_product, name='admin-update-product'),
--- api/views/product_views.py
+++ api/views/product_views.py
@@ -3,15 +3,39 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import status
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 from products.models import Product, Category, Review
 from api.serializers import ProductSerializer
 
 @api_view(['GET'])
 def get_products(request):
-    products = Product.objects.all()
+    query = request.query_params.get('keyword')
+    if query == None:
+        query = ''
+
+    # page = request.query_params.get('page')
+    # paginator = Paginator(products, 5)
+
+    # try:
+    #     products = paginator.page(page)
+    # except PageNotAnInteger:
+    #     products = paginator.page(1)
+    # except EmptyPage:
+    #     products = paginator.page(paginator.num_pages)
+
+    # if page == None:
+    #     page = 1
+
+    # page = int(page)
+
+
+
+    products = Product.objects.filter(name__icontains=query)
     serializer = ProductSerializer(products, many=True)
 
     return Response(serializer.data)
+    # return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
 
 
 @api_view(['GET'])
@@ -90,6 +114,31 @@ def upload_product_image(request):
         message = {'derail': 'Product not found'}
         return Response(message, status=status.HTTP_404_NOT_FOUND)
 
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
-# def 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_product_review(request, product_id):
+    user = request.user
+    product = Product.objects.get(id=product_id)
+    data = request.data
+
+    # (1) Review already exists
+    already_exists = Product.review_set.filter(user=user).exists()
+    if already_exists:
+        message = {'detail': 'product already reviewed'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+    # (2) No rating or 0
+    elif data['rating'] == 0:
+        message = {'detail': 'please select a rating'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
+    # (3) Create Review
+    else:
+        review = Review.objects.create(
+            product=product,
+            user=user,
+            name=user.full_name,
+            rating=data['rating'],
+            comment=data['comment']
+        )
+        return Response('review added successfully')
